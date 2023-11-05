177509c49028db827f4970df9ce3017ff5a2fbac
kouhei.umeki <kouhei11.u@gmail.com>
2023-11-05 01:17:37 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include SessionHelper
+  include SessionsHelper
   include Pagy::Backend
   
   private
@@ -8,5 +8,9 @@ class ApplicationController < ActionController::Base
     unless logged_in?
       redirect_to login_url
     end
+  end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
   end
 end
